661efea7502c25eee25115eb1429d5c8a41af002
NikGabric <36707304+NikGabric@users.noreply.github.com>
2023-11-22 14:52:45 +0100

    Day 8 part 2

--- src/tasks/eight.rs
+++ src/tasks/eight.rs
@@ -69,6 +69,53 @@ pub fn part_two() {
     let file_path = "./src/tasks/inputs/eight.txt";
     let text = fs::read_to_string(file_path)
         .expect("Not able to read the file");
+
+    let mut grid = init_grid(text);
+    let mut max: i32 = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let (mut up, mut right, mut down, mut left) = (0, 0, 0, 0);
+            // Up
+            if i > 0 {
+                for k in (0..i).rev() {
+                    up += 1;
+                    if grid[i][j] <= grid[k][j] {
+                        break;
+                    }
+                }
+            }
+            // Right
+            for k in (j + 1)..grid[i].len() {
+                right += 1;
+                if grid[i][j] <= grid[i][k] {
+                    break;
+                }
+            }
+            // Down
+            for k in (i + 1)..grid.len() {
+                down += 1;
+                if grid[i][j] <= grid[k][j] {
+                    break;
+                }
+            }
+            // Left
+            if j > 0 {
+                for k in (0..j).rev(){
+                    left += 1;
+                    if grid[i][j] <= grid[i][k] {
+                        break;
+                    }
+                }
+            }
+            let scenic_score = up * right * down * left;
+            if scenic_score > max {
+                max = scenic_score;
+            }
+        }
+    }
+
+    println!("{max}");
 }
 
 fn init_grid(text: String) -> Vec<Vec<i8>> {
