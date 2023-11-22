60dc00927045af7107404fd16d0f933cb7f4f08c
NikGabric <36707304+NikGabric@users.noreply.github.com>
2023-11-22 17:43:49 +0100

    Day 9 refactor and part 2

--- src/tasks/nine.rs
+++ src/tasks/nine.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::collections::HashSet;
 
-#[derive(Eq, Hash, PartialEq, Debug)]
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone)]
 struct Coords {
     x: i32,
     y: i32,
@@ -12,6 +12,15 @@ impl Coords {
         Self {
             x: 0,
             y: 0,
+        }
+    }
+
+    fn update(&mut self, h: Coords) {
+        let dx = h.x - self.x;
+        let dy = h.y - self.y;
+        if (dx.abs() + dy.abs() > 1) && (dx.abs() > 1 || dy.abs() > 1) {
+            self.x += dx.signum();
+            self.y += dy.signum();
         }
     }
 }
@@ -32,40 +41,16 @@ pub fn part_one() {
         let mut amount: i32 = split_line.1.parse::<i32>().unwrap();
         while amount > 0 {
             match dir {
-                "U" => {
-                    h.y += 1;
-                    if h.y > t.y + 1 {
-                        t.y += 1;
-                        t.x = h.x;
-                        visited.insert(Coords {x: t.x, y: t.y});
-                    }
-                }
-                "R" => {
-                    h.x += 1;
-                    if h.x > t.x + 1 {
-                        t.x += 1;
-                        t.y = h.y;
-                        visited.insert(Coords {x: t.x, y: t.y});
-                    }
-                }
-                "D" => {
-                    h.y -= 1;
-                    if h.y < t.y - 1 {
-                        t.y -= 1;
-                        t.x = h.x;
-                        visited.insert(Coords {x: t.x, y: t.y});
-                    }
-                }
-                "L" => {
-                    h.x -= 1;
-                    if h.x < t.x - 1 {
-                        t.x -= 1;
-                        t.y = h.y;
-                        visited.insert(Coords {x: t.x, y: t.y});
-                    }
-                }
+                "U" => h.y += 1,
+                "R" => h.x += 1,
+                "D" => h.y -= 1,
+                "L" => h.x -= 1,
                 _ => {}
             }
+
+            t.update(h);
+            visited.insert(t);
+
             amount -= 1;
         }
     }
@@ -77,4 +62,33 @@ pub fn part_two() {
     let file_path = "./src/tasks/inputs/nine.txt";
     let text = fs::read_to_string(file_path)
         .expect("Not able to read the file");
+
+    let mut snake: Vec<Coords> = vec![Coords::new(); 10];
+    let mut visited: HashSet<Coords> = HashSet::new();
+    visited.insert(snake[9]);
+
+    for line in text.lines() {
+        let split_line = line.split_once(" ").unwrap();
+        let dir: &str = split_line.0;
+        let mut amount: i32 = split_line.1.parse::<i32>().unwrap();
+        while amount > 0 {
+            match dir {
+                "U" => snake[0].y += 1,
+                "R" => snake[0].x += 1,
+                "D" => snake[0].y -= 1,
+                "L" => snake[0].x -= 1,
+                _ => {}
+            }
+
+            for i in 1..snake.len() {
+                let lead = snake[i-1].clone();
+                snake[i].update(lead);
+            }
+            
+            visited.insert(snake[9]);
+            amount -= 1;
+        }
+    }
+
+    println!("{}", visited.len());
 }
